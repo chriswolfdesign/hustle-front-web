b106cf3233932eb04669b1f90fbc4c960afa0c45
Chris Wolf <chriswolfdesign@gmail.com>
2021-11-13 09:39:03 -0500

    Added space for an individual staff part

---
+++ src/components/part/Part.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from "react";
+import "./Part.css";
+
+function Part(): ReactElement {
+    return <div className="Part">Part</div>;
+}
+
+export default Part;
--- src/components/score/Score.tsx
+++ src/components/score/Score.tsx
@@ -1,11 +1,13 @@
 import React, { ReactElement } from "react";
 import "./Score.css";
 import ScoreHeader from "../score_header/ScoreHeader";
+import Part from "../part/Part";
 
 function Score(): ReactElement {
     return (
         <div className="Score">
             <ScoreHeader title="My Composition" composer="Wolf" />
+            <Part />
         </div>
     );
 }
