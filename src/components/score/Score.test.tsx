6c0fd2ebef67b44a4157049b2f7f593d3a006563
Chris Wolf <chriswolfdesign@gmail.com>
2021-11-13 07:30:21 -0500

    Updated tests for Score just to look for a ScoreHeader

--- src/components/score/Score.test.tsx
+++ src/components/score/Score.test.tsx
@@ -3,7 +3,6 @@ import { render, screen } from "@testing-library/react";
 import Score from "./Score";
 
 test("renders a default project", () => {
-    render(<Score />);
-    const title = screen.getByText(/Untitled/i);
-    expect(title).toBeInTheDocument();
+    const { container } = render(<Score />);
+    expect(container.getElementsByClassName("ScoreHeader").length).toBe(1)
 });
