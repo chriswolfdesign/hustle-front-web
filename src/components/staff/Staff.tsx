d6640372b62d0a32d0b056cc6f4ad52731b941be
chriswolfdesign <chriswolfdesign@gmail.com>
2021-11-23 06:38:11 -0500

    Added the top line of the staff

--- src/components/staff/Staff.tsx
+++ src/components/staff/Staff.tsx
@@ -1,8 +1,27 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useRef } from "react";
 import "./Staff.css";
 
 function Staff(): ReactElement {
-    return <div className="Staff">Staff will go here</div>;
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const canvasCtxRef = React.useRef<CanvasRenderingContext2D | null>(null);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            const HEIGHT = canvasRef.current.height;
+            const WIDTH = canvasRef.current.width;
+            const LINE_THICKNESS = 3;
+
+            canvasCtxRef.current = canvasRef.current.getContext("2d");
+            const ctx = canvasCtxRef.current;
+            ctx!.fillRect(0, 0, canvasRef.current.width, LINE_THICKNESS);
+        }
+    }, []);
+
+    return (
+        <div id="Staff">
+            <canvas ref={canvasRef}></canvas>
+        </div>
+    );
 }
 
 export default Staff;
