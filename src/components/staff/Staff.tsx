c92d8eeefb5758a706c8f833d2a9d7fae5409ac2
Chris Wolf <chriswolfdesign@gmail.com>
2021-12-05 09:00:03 -0500

    Canvas staff (#26)
    * Added the top line of the staff
    
    * Added the bottom line of the staff
    
    * Added bottom line of staff
    
    * Added remaining lines
    
    * Refactored line drawing into its own function
    
    * Refactored each line draw into its own function
    
    * Fixed unit tests

--- src/components/staff/Staff.tsx
+++ src/components/staff/Staff.tsx
@@ -1,8 +1,108 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useRef } from "react";
 import "./Staff.css";
 
 function Staff(): ReactElement {
-    return <div className="Staff">Staff will go here</div>;
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const canvasCtxRef = React.useRef<CanvasRenderingContext2D | null>(null);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            const HEIGHT = canvasRef.current.height;
+            const WIDTH = canvasRef.current.width;
+            const LINE_THICKNESS = 3;
+
+            canvasCtxRef.current = canvasRef.current.getContext("2d");
+
+            if (canvasCtxRef.current !== null) {
+                const ctx = canvasCtxRef.current;
+
+                drawStaffLines(ctx, HEIGHT, WIDTH, LINE_THICKNESS);
+            }
+        }
+    }, []);
+
+    return (
+        <div id="Staff">
+            <canvas ref={canvasRef}></canvas>
+        </div>
+    );
+}
+
+function drawStaffLines(
+    ctx: CanvasRenderingContext2D | null,
+    height: number,
+    width: number,
+    line_thickness: number,
+) {
+    drawTopStaffLine(ctx, width, line_thickness);
+    drawSecondFromTopStaffLine(ctx, height, width, line_thickness);
+    drawMiddleStaffLine(ctx, height, width, line_thickness);
+    drawSecondFromBottomStaffLine(ctx, height, width, line_thickness);
+    drawBottomStaffLine(ctx, height, width, line_thickness);
+}
+
+function drawTopStaffLine(
+    ctx: CanvasRenderingContext2D | null,
+    width: number,
+    line_thickness: number,
+) {
+    if (ctx !== null) {
+        ctx.fillRect(0, 0, width, line_thickness);
+    }
+}
+
+function drawSecondFromTopStaffLine(
+    ctx: CanvasRenderingContext2D | null,
+    height: number,
+    width: number,
+    line_thickness: number,
+) {
+    if (ctx !== null) {
+        ctx.fillRect(
+            0,
+            height * 0.25 - line_thickness / 2,
+            width,
+            line_thickness,
+        );
+    }
+}
+
+function drawMiddleStaffLine(
+    ctx: CanvasRenderingContext2D | null,
+    height: number,
+    width: number,
+    line_thickness: number,
+) {
+    if (ctx !== null) {
+        ctx.fillRect(0, height / 2 - line_thickness / 2, width, line_thickness);
+    }
+}
+
+function drawSecondFromBottomStaffLine(
+    ctx: CanvasRenderingContext2D | null,
+    height: number,
+    width: number,
+    line_thickness: number,
+) {
+    if (ctx !== null) {
+        ctx.fillRect(
+            0,
+            height * 0.75 - line_thickness / 2,
+            width,
+            line_thickness,
+        );
+    }
+}
+
+function drawBottomStaffLine(
+    ctx: CanvasRenderingContext2D | null,
+    height: number,
+    width: number,
+    line_thickness: number,
+) {
+    if (ctx !== null) {
+        ctx.fillRect(0, height - line_thickness, width, line_thickness);
+    }
 }
 
 export default Staff;
